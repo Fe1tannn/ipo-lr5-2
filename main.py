e68c1b63bbacaf6a7c33ff4531099db87d6906f6
Yan <tal531764@gmail.com>
2024-12-10 01:14:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-a = str(input("Введите первую строку: "))
-b = str(input("Введите вторую строку: "))
-rep1 = a.replace(" ","")
-rep2 = b.replace(" ","")
-len1 = len(rep1)
-len2 = len(rep2)
-count = 0
-for i in rep1.lower():
-    if i in rep2.lower():
+a = str(input("Введите первую строку: ")) #Запрашиваем первую строку
+b = str(input("Введите вторую строку: ")) #Запрашиваем вторую строку
+rep1 = a.replace(" ","") #Заменяем пробелы (удаляем)
+rep2 = b.replace(" ","") #Заменяем пробелы (удаляем)
+len1 = len(rep1) #Ищем длинну
+len2 = len(rep2) #Ищем длинну
+count = 0 
+for i in rep1.lower(): #Перебор i в rep1
+    if i in rep2.lower(): #Перебо i в rep2
         count += 1
-if count == len1 and len1 == len2:
+if count == len1 and len1 == len2: #Условие
     print("Является анаграммой ")
 else:
     print("Неявляется анаграммой ")
